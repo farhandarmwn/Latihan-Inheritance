b742c599771d51fc09db24ff04d45f29b34c90d5
Muhammad Farhan <48467625+farhandarmwn@users.noreply.github.com>
2019-05-07 14:45:41 +0700

    Add files via upload

---
+++ Program + Tugas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Drake", 66, "11115555", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Miss", 45, "160800", "Misssalah@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
