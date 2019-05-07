3e9c912437418f59106b953a8a337e429c38e685
Muhammad Farhan <48467625+farhandarmwn@users.noreply.github.com>
2019-05-07 14:13:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 20;
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher();
+            teacher.Name = "Drake";
+            teacher.Age = 66;
+            teacher.TeacherId = "11115555";
+            teacher.Subject = "Math";
+            teacher.GetNameAndAge();
+
+            Student student = new Student();
+            student.Name = "Miss";
+            student.Age = 45;
+            student.StudentId = "160800";
+            student.Email = "Misssalah@gmail.com";
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
